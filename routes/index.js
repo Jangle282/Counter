522067279fc82bd8fccdb01e438d2e455cf106ae
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-02-11 17:38:10 +0100

    add data capture view and functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router  = express.Router();
+const Project = require("../models/Project")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/data-capture', (req, res, next) => {
+  Project.find()
+    .then(projects => {
+      res.render('data-capture', {projects});
+    })
+    .catch(err => next(err))
 });
 
 module.exports = router;
