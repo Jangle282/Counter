30dee061bbd8797365fc90a8b288ee6997d41a6a
Jengle <jennyrob282@gmail.com>
2019-02-14 12:53:29 +0100

    finished edit profile and project pages added cancel buttons

--- routes/auth.js
+++ routes/auth.js
@@ -25,6 +25,41 @@ router.post("/login", passport.authenticate("local", {
   passReqToCallback: true
 }));
 
+//-----GOOGLE CRAP----
+passport.use(new GoogleStrategy({
+  clientID: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+  callbackURL: "http://localhost:3000/auth/google/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  console.log("I am in the strategy", accessToken, refreshToken, profile)
+  // look if that user exists
+  // if user exists, redirect to main page
+  // otherwise, create a new user
+  // then redirect to main page
+
+  //J need to find a way to get the email address of the user. Perhaps need to verify th domain like Naaman. 
+  User.findOne({ email: "Kiarra.Doyle@darwin.net" }, (err, user) => {
+    console.log("User", user)
+    return done(null, user);
+  })
+  
+  //User.findOrCreate({ googleId: profile.id }, function (err, user) {
+  //  return done(err, user);
+  //});
+}
+));
+
+router.get('/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }
+  ));
+
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/auth/login' }),
+  function(req, res) {
+    console.log("In google callback")
+    res.redirect('/');
+  });
 
 router.get("/signup", isNotConnected, (req, res, next) => {
   Project.find()
@@ -107,42 +142,5 @@ router.get("/logout", isConnected, (req, res) => {
   req.logout();
   res.redirect("/");
 });
-
-//-----GOOGLE CRAP----
-passport.use(new GoogleStrategy({
-  clientID: process.env.clientId,
-  clientSecret: process.env.clientSecret,
-  callbackURL: "http://localhost:3000/auth/google/callback"
-},
-function(accessToken, refreshToken, profile, done) {
-  console.log("I am in the strategy", accessToken, refreshToken, profile)
-  // look if that user exists
-  // if user exists, redirect to main page
-  // otherwise, create a new user
-  // then redirect to main page
-
-  //J need to find a way to get the email address of the user. Perhaps need to verify th domain like Naaman. 
-  User.findOne({ email: "Kiarra.Doyle@darwin.net" }, (err, user) => {
-    console.log("User", user)
-    return done(null, user);
-  })
-  
-  //User.findOrCreate({ googleId: profile.id }, function (err, user) {
-  //  return done(err, user);
-  //});
-}
-));
-
-router.get('/google',
-  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }
-  ));
-
-router.get('/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/auth/login' }),
-  function(req, res) {
-    console.log("In google callback")
-    res.redirect('/');
-  });
-
 
 module.exports = router;
